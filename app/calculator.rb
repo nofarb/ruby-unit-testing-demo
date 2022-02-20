d932625223b78e665fb8ae60fdf80f6b5b76a589
Genesis Ivan Ilagan <ivanilagan1109@gmail.com>
2022-02-21 00:30:24 +0800

    Unit Test CleanUp & Minor Code Improvements

--- app/calculator.rb
+++ app/calculator.rb
@@ -1,6 +1,12 @@
 class Calculator
   def self.sum(num1, num2)
-    raise NotSupportedData if !num1.instance_of?(Fixnum) || !num2.instance_of?(Fixnum)
+    raise NotSupportedData if !is_valid_number?(num1) || !is_valid_number?(num2)
     num1 + num2
+  end
+
+  private
+
+  def self.is_valid_number?(num)
+    num.instance_of?(Fixnum) || num.instance_of?(Float)
   end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,16 +2,16 @@ require './app/calculator'
 require './app/custom_exceptions/not_supported_data'
 
 RSpec.describe Calculator do
-  describe "#sum" do
-    context "when sum method is called" do
+  describe "#sum(num1, num2)" do
+    context "when inputs are numbers" do
       it "returns the sum of 2 numbers" do
         expect(Calculator.sum(1,2)).to eq 3
       end
+    end
 
-      context "when input is NOT a number" do
-        it "returns a NotSupportedData exception" do
-          expect { Calculator.sum("a", 2) }.to raise_error(NotSupportedData)
-        end
+    context "when 1 or 2 inputs is NOT a number" do
+      it "returns a NotSupportedData exception" do
+        expect { Calculator.sum("a", 2) }.to raise_error(NotSupportedData)
       end
     end
   end
